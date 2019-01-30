48118a7d18533e26a8b92b4299df1f796c2e4123
harrebarre <maxilos84@gmail.com>
2019-01-30 16:55:03 +0100

    Implement obedient ai controllable by user input

---
+++ WallAI.Simulation/Ai/ObedientAi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using WallAI.Core.Ai;
+using WallAI.Core.Enums;
+
+namespace WallAI.Simulation.Ai
+{
+    public class ObedientAi : IAi
+    {
+        public void Tick(IAiCore ai)
+        {
+            if (ai.Stats.Energy == 0)
+            {
+                ai.Kill();
+                return;
+            }
+
+            ConsoleKey? key = null;
+            while (Console.KeyAvailable)
+                key = Console.ReadKey(false).Key;
+
+            if (key != null)
+            {
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        ai.Move(Direction.North);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        ai.Move(Direction.South);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        ai.Move(Direction.West);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        ai.Move(Direction.East);
+                        break;
+                }
+            }
+        }
+    }
+}
