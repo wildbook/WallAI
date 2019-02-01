ba89802b74ca5f417ac3aea3edbf00086dc06281
William Book <book.wille@gmail.com>
2019-02-01 08:02:48 +0100

    Refactor World2D

--- WallAI.Core/Worlds/World2D.cs
+++ WallAI.Core/Worlds/World2D.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using WallAI.Core.Math.Geometry;
 using WallAI.Core.Tiles;
 using WallAI.Core.Worlds.Ai;
@@ -10,103 +11,90 @@ using WallAI.Core.Worlds.Tiles;
 
 namespace WallAI.Core.Worlds
 {
+    [DataContract]
     public class World2D : IWorld2D
     {
-        public int Seed { get; }
-
         private readonly IWorld2DMethods _methods;
-        internal readonly ConcurrentDictionary<Point2D, ITile2D> Tiles;
+        private readonly ConcurrentDictionary<Point2D, ITile2D> _tiles;
 
-        private IEnumerable<World2DEntity> Entities
-            => Tiles.Where(x => x.Value.Entity != null)
-                    .Select(x => new World2DEntity(this, x.Value.Entity, x.Key));
+        [DataMember]
+        public IEnumerable<IWorld2DEntity> Entities
+            => _tiles.Where(x => x.Value.Entity != null)
+                     .Select(x => new World2DEntity(this, x.Value.Entity, x.Key));
 
-        private IEnumerable<IWorld2DTile2D> TilesInRange(Circle2D circle)
-        {
-            for (var x = -circle.Radius; x < circle.Radius; x++)
-            for (var y = -circle.Radius; y < circle.Radius; y++)
-            {
-                var point = circle.Origin + new Point2D(x, y);
-                if (circle.ContainsPoint(point))
-                    yield return new World2DTile2D(this[point], point, this);
-            }
-        }
+        [DataMember] public int Seed { get; }
 
-        private IEnumerable<IWorld2DTile2D> TilesInRect(Rectangle2D rect)
-        {
-            for (var x = rect.X1; x < rect.X2; x++)
-            for (var y = rect.Y1; y < rect.Y2; y++)
-            {
-                var point = new Point2D(x, y);
-                if (rect.ContainsPoint(point))
-                    yield return new World2DTile2D(this[point], point, this);
-            }
-        }
-
-        IReadOnlyPartialWorld2D IReadOnlyWorld2D.CreateDerivedWorld2D(Point2D center, Rectangle2D worldRect, Func<Point2D, bool> isVisible)
-            => CreateDerivedWorld2D(center, worldRect, isVisible);
+        public static IWorld2D Create(IWorld2DMethods methods, int seed)
+            => new World2D(methods, seed);
 
         public IPartialWorld2D CreateDerivedWorld2D(Point2D center, Rectangle2D worldRect, Func<Point2D, bool> isVisible)
             => new PartialWorld2D(this, center, worldRect, isVisible);
 
-        IEnumerable<IWorld2DEntity> IWorld2D.Entities => Entities;
-        IEnumerable<IReadOnlyWorld2DEntity> IReadOnlyWorld2D.Entities => Entities;
+        public void Dispose() { }
 
-        IEnumerable<IWorld2DTile2D> IWorld2D.TilesInRange(Circle2D circle) => TilesInRange(circle);
-        IEnumerable<IReadOnlyWorld2DTile2D> IReadOnlyWorld2D.TilesInRange(Circle2D circle) => TilesInRange(circle);
-
-        IEnumerable<IWorld2DTile2D> IWorld2D.TilesInRect(Rectangle2D rect) => TilesInRect(rect);
-        IEnumerable<IReadOnlyWorld2DTile2D> IReadOnlyWorld2D.TilesInRect(Rectangle2D rect) => TilesInRect(rect);
-
-        IReadOnlyTile2D IReadOnlyWorld2D.this[Point2D location] => this[location];
-        ITile2D IWorld2D.this[Point2D location]
+        public IEnumerable<IWorld2DTile2D> TilesInRange(Circle2D circle)
         {
-            get => this[location];
-            set => this[location] = value;
+            for (var x = -circle.Radius; x < circle.Radius; x++)
+                for (var y = -circle.Radius; y < circle.Radius; y++)
+                {
+                    var point = circle.Origin + new Point2D(x, y);
+                    if (circle.ContainsPoint(point))
+                        yield return new World2DTile2D(this[point], point, this);
+                }
+        }
+
+        public IEnumerable<IWorld2DTile2D> TilesInRect(Rectangle2D rect)
+        {
+            for (var x = rect.X1; x < rect.X2; x++)
+                for (var y = rect.Y1; y < rect.Y2; y++)
+                {
+                    var point = new Point2D(x, y);
+                    if (rect.ContainsPoint(point))
+                        yield return new World2DTile2D(this[point], point, this);
+                }
         }
 
         protected internal virtual ITile2D this[Point2D coordinate]
         {
             get
             {
-                if (Tiles.TryGetValue(coordinate, out var tile))
+                if (_tiles.TryGetValue(coordinate, out var tile))
                     return tile;
 
                 var generatedTile = _methods.GenerateTile(Seed, coordinate);
-
-                //if (generatedTile.Entity == null)
-                //    return new TemporaryTile2D(generatedTile, z => this[coordinate] = z);
-
                 return this[coordinate] = generatedTile;
             }
             set
             {
-                //var defaultTile = _methods.GenerateTile(Seed, coordinate);
-                //
-                //if (defaultTile.Entity == null && Equals(defaultTile, value))
-                //{
-                //    if (!Tiles.TryRemove(coordinate, out _))
-                //        throw new Exception("Failed to remove non-modified tile.");
-                //}
-                //else
-                //{
                 if (value != null)
-                    Tiles[coordinate] = value;
-                else if (!Tiles.TryRemove(coordinate, out _))
+                    _tiles[coordinate] = value;
+                else if (!_tiles.TryRemove(coordinate, out _))
                     throw new Exception();
-                //}
             }
         }
-
-        public static IWorld2D Create(IWorld2DMethods methods, int seed) => new World2D(methods, seed);
 
         protected World2D(IWorld2DMethods methods, int seed)
         {
             Seed = seed;
             _methods = methods;
-            Tiles = new ConcurrentDictionary<Point2D, ITile2D>();
+            _tiles = new ConcurrentDictionary<Point2D, ITile2D>();
         }
 
-        public void Dispose() { }
+        int IWorld2D.Seed => Seed;
+
+        IEnumerable<IWorld2DEntity> IWorld2D.Entities => Entities;
+        IEnumerable<IReadOnlyWorld2DEntity> IReadOnlyWorld2D.Entities => Entities;
+
+        ITile2D IWorld2D.this[Point2D location] { get => this[location]; set => this[location] = value; }
+        IReadOnlyTile2D IReadOnlyWorld2D.this[Point2D location] => this[location];
+
+        IPartialWorld2D IWorld2D.CreateDerivedWorld2D(Point2D center, Rectangle2D worldRect, Func<Point2D, bool> isVisible) => CreateDerivedWorld2D(center, worldRect, isVisible);
+        IReadOnlyPartialWorld2D IReadOnlyWorld2D.CreateDerivedWorld2D(Point2D center, Rectangle2D worldRect, Func<Point2D, bool> isVisible) => CreateDerivedWorld2D(center, worldRect, isVisible);
+
+        IEnumerable<IWorld2DTile2D> IWorld2D.TilesInRange(Circle2D circle) => TilesInRange(circle);
+        IEnumerable<IReadOnlyWorld2DTile2D> IReadOnlyWorld2D.TilesInRange(Circle2D circle) => TilesInRange(circle);
+
+        IEnumerable<IWorld2DTile2D> IWorld2D.TilesInRect(Rectangle2D rect) => TilesInRect(rect);
+        IEnumerable<IReadOnlyWorld2DTile2D> IReadOnlyWorld2D.TilesInRect(Rectangle2D rect) => TilesInRect(rect);
     }
 }
